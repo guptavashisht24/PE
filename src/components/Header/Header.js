2a72d0496b718a18d07a89a8ec30e4789c6e536b
Vashisht Gupta <guptavashisht24@gmail.com>
2020-01-12 16:36:13 +0530

    add classNames

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,45 +7,22 @@ import {
 const Header = () => {
 
   const toggleMenu = () => {
-     console.log("imma clicked")
     let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-mainNav.classList.toggle('active');
+    mainNav.classList.toggle('active');
   }
 
   return (
-    // <nav className = "navBar">
-    //   <div className = "container">
-    //     <div className = "logo">
-    //       <img src = {logo} alt = "PANKAJ ELECTRICALS"></img>
-    //     </div>
-    //     <div className = "navList headerShow" id="lists"> 
-    //       <ul>
-    //         <li><Link to="/">Home</Link></li>
-    //         <li><Link to="/about">About Us</Link></li>
-    //         <li>Products</li>
-    //       </ul>
-    //     </div>
-    //     <div className = "showButton hamburger" onClick = {toggleMenu}><span className="bar"></span><span className="bar mt4"></span><span className="bar mt4"></span></div>
-    //   </div>
-    // </nav>
-    <nav class="navbar">
-      <span class="navbar-toggle" id="js-navbar-toggle" onClick = {toggleMenu}>
+    <nav className="navbar">
+      <span className="navbar-toggle" id="js-navbar-toggle" onClick = {toggleMenu}>
         <span className="bar"></span>
         <span className="bar mt4"></span>
         <span className="bar mt4"></span>
       </span>
-      <a href="#" class="logo"><img src = {logo} alt = "PANKAJ ELECTRICALS"></img></a>
-      <ul class="main-nav" id="js-menu">
-        <li>
-          <a href="#" class="nav-links">Home</a>
-        </li>
-        <li>
-          <a href="#" class="nav-links">Products</a>
-        </li>
-        <li>
-          <a href="#" class="nav-links">About Us</a>
-        </li>
+      <a href="#" className="logo"><img src = {logo} alt = "PANKAJ ELECTRICALS"></img></a>
+      <ul className="main-nav" id="js-menu">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/about">Products</Link></li>
       </ul>
     </nav>
   )
