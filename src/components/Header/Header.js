92a845ad980d69c12b2bfaefec4cdb35eba03d95
Vashisht Gupta <guptavashisht24@gmail.com>
2020-01-12 16:11:01 +0530

    add data

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 
 const Header = () => {
-  
+
   const toggleMenu = () => {
     console.log("Imma clicked")
     var element = document.getElementById("lists");
@@ -13,20 +13,23 @@ const Header = () => {
   }
 
   return (
-    <nav className = "navBar">
-      <div className = "container">
-        <div className = "logo">
-          <img src = {logo} alt = "PANKAJ ELECTRICALS"></img>
-        </div>
-        <div className = "navList headerShow" id="lists"> 
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li>Products</li>
-          </ul>
-        </div>
-        <div className = "showButton hamburger" onClick = {toggleMenu}><span className="bar"></span><span className="bar mt4"></span><span className="bar mt4"></span></div>
+    <nav className="navBar">
+      <div>
+      <a>
+        <img src={logo} alt="PANKAJ ELECTRICALS"></img>
+      </a>
       </div>
+      <div className="navList headerShow" id="lists">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li>Products</li>
+        </ul>
+      </div>
+      <div className="showButton hamburger" onClick={toggleMenu}>
+      <span className="bar"></span>
+      <span className="bar mt4"></span>
+      <span className="bar mt4"></span></div>
     </nav>
   )
 }
