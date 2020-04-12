674fce8e816a70148edd48fb5c9eb736d14ca010
Vashisht Gupta <guptavashisht24@gmail.com>
2020-04-12 16:57:51 +0530

    add map iframe

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,19 +1,11 @@
-import React, { useEffect } from 'react';
-
-
+import React from 'react';
+import MapContainer from './Map';
 const Contact = () => {
-  // const initMap = () => {
-  //   var uluru = {lat: -25.344, lng: 131.036};
-  //   var map = new google.maps.Map(
-  //       document.getElementById('map'), {zoom: 4, center: uluru});
-  //   var marker = new google.maps.Marker({position: uluru, map: map});
-  //   }
 
-  useEffect(()=>{
-    //initMap()
-  },[])
- return(<div id="map"></div>)
-  
-  }
+
+
+  return (<MapContainer />)
+
+}
 
 export default Contact
